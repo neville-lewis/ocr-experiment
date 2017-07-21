4451ebe98c4f4c2188fc971e86ee0c22cab960d8
Lewis <Neville.Lewis@titletec.com>
2017-07-20 19:03:58 -0600

    using asprise OCR

--- DocumentScraper/Ocr.Engine/ScanImage.cs
+++ DocumentScraper/Ocr.Engine/ScanImage.cs
@@ -4,25 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using asprise_ocr_api;
+
 namespace Ocr.Engine
 {
     public class ScanImage : StepChain
     {
 
         private List<string> _scanWords ;
-
+        private AspriseOCR _ocr;
 
         public ScanImage()
         {
             _scanWords = new List<string>();
             _scanWords.Add("MV-34");
             _scanWords.Add("MV-34");
+
+            AspriseOCR.SetUp();
+            _ocr = new AspriseOCR();
+            _ocr.StartEngine("eng", AspriseOCR.SPEED_FASTEST);
         }
 
         public override void Process(List<string> files)
         {
-            //get files from previous step
-            List<string> str =files;
 
             //perform scan action for each file
             foreach(string file in files)
@@ -30,16 +34,23 @@ namespace Ocr.Engine
                 searchKeyWord(file);
                 CallNextStep();
             }
-            
+
+            _ocr.StopEngine();
         }
 
 
         private bool searchKeyWord(string imageFile)
         {
-            bool isFound = false;
+            string dataExtracted;
 
+            //run thru each of the keywords
+            foreach (string scanWord in _scanWords)
+            {
+                dataExtracted = _ocr.Recognize(imageFile, -1, -1, -1, -1, -1, AspriseOCR.RECOGNIZE_TYPE_ALL, AspriseOCR.OUTPUT_FORMAT_PLAINTEXT);
+                if (dataExtracted.Contains(scanWord)) { return true; }
+            }
 
-            return isFound;
+            return false;
         }
     }
 }
